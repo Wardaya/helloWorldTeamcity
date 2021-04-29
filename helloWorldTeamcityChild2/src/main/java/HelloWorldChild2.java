5261e82a1f9a3d6c988f23e54d0909aabb39af0c
Farah <DreamHigh181993!>
2021-04-29 16:01:04 +0200

    change text 29042021

--- helloWorldTeamcityChild2/src/main/java/HelloWorldChild2.java
+++ helloWorldTeamcityChild2/src/main/java/HelloWorldChild2.java
@@ -1,6 +1,6 @@
 public class HelloWorldChild2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world teamcity!! Test Trigger 02");
+        System.out.println("Hello world teamcity!! Test Trigger 29.04.2021");
     }
 }
